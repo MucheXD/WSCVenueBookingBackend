3210aadaf1fc665f7d70a6ada1c04ccb4ab80c20
MucheXD <68536431+MucheXD@users.noreply.github.com>
2026-02-06 22:12:51 +0800

    refactor: 封装jwt组件并使暴露接口对称

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -3,6 +3,7 @@ package middlewares
 import (
 	"strings"
 
+	"github.com/MucheXD/WSCVenueBookingBackend/internal/apiException"
 	"github.com/MucheXD/WSCVenueBookingBackend/internal/utils"
 	"github.com/gin-gonic/gin"
 )
@@ -12,46 +13,21 @@ func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		authHeader := c.GetHeader("Authorization")
 		if authHeader == "" {
-			c.JSON(401, gin.H{
-				"code": 401,
-				"msg":  "请先登录",
-				"data": nil,
-			})
-			c.Abort()
+			apiException.AbortWithException(c, apiException.Unauthorized)
 			return
 		}
-		parts:= strings.SplitN(authHeader, " ", 2)
-		if len(parts) != 2 || parts[0]!="Bearer" {
-			c.JSON(401, gin.H{
-				"code": 401,
-				"msg":  "登录异常，请重新登录",
-				"data": nil,
-			})
-			c.Abort()
+		parts := strings.SplitN(authHeader, " ", 2)
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			apiException.AbortWithException(c, apiException.AuthInvalid)
 			return
 		}
 		tokenString := parts[1]
-		token, err := utils.ParseToken(tokenString)
+		tokenData, err := utils.GetTokenData(tokenString)
 		if err != nil {
-			c.JSON(401, gin.H{
-				"code": 401,
-				"msg":  "登录异常，请重新登录",
-				"data": nil,
-			})
-			c.Abort()
+			apiException.AbortWithException(c, apiException.AuthInvalid)
 			return
 		}
-		claims, err := utils.ExtractClaims(token)
-		if err != nil {
-			c.JSON(401, gin.H{
-				"code": 401,
-				"msg":  "登录异常，请重新登录",
-				"data": nil,
-			})
-			c.Abort()
-			return
-		}
-		c.Set("user_id", claims["user_id"])
+		c.Set("user_id", tokenData.UserID)
 		c.Next()
 	}
 }
--- internal/utils/jwt.go
+++ internal/utils/jwt.go
@@ -7,42 +7,47 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
-// 生成token
-func GenerateToken(userID uint) (string, error) {
-	claims:=jwt.MapClaims{
-        "user_id":userID,
-        "exp":time.Now().Add(time.Second*time.Duration(config.Config.GetInt("jwt.expiration"))).Unix(),
-        "iat":time.Now().Unix(),
-        "iss":"gin_jwt_demo",
-        "nbf":time.Now().Unix(),
-    }
-    token:=jwt.NewWithClaims(jwt.SigningMethodHS256,claims)
-    tokenStr,err:=token.SignedString([]byte(config.Config.GetString("jwt.secret")))
-    if err!=nil{
-        return "",err
-    }
-    return tokenStr,nil
+type TokenData struct {
+	UserID             uint `json:"uid"`
+	VenueAccessGroupID uint `json:"vag,omitempty"`
+}
+type tokenClaims struct {
+	TokenData
+	jwt.RegisteredClaims
 }
 
-//验证token
-func ParseToken(tokenStr string) (*jwt.Token, error) {
-    //检查签名方法是否正确
-    token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
-        if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-            return nil,jwt.ErrSignatureInvalid
-        }
-        return []byte(config.Config.GetString("jwt.secret")), nil
-    })
-        
-    if err != nil {
-        return nil,err
-    }
-	return token,nil
+// 生成token
+func GenerateToken(tokenData TokenData) (string, error) {
+	claims := tokenClaims{
+		TokenData: tokenData,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Second * time.Duration(config.Config.GetInt("jwt.expiration")))),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenStr, err := token.SignedString([]byte(config.Config.GetString("jwt.secret")))
+	if err != nil {
+		return "", err
+	}
+	return tokenStr, nil
 }
-func ExtractClaims(token *jwt.Token)(jwt.MapClaims,error){
-    if claims,ok:=token.Claims.(jwt.MapClaims);ok&&token.Valid{
-        return claims,nil
-    }else{
-        return nil,jwt.ErrTokenInvalidClaims 
-    }
+
+func GetTokenPayload(tokenStr string) (TokenData, error) {
+	var claims tokenClaims
+	token, err := jwt.ParseWithClaims(tokenStr, &claims, func(token *jwt.Token) (any, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, jwt.ErrSignatureInvalid
+		}
+		return []byte(config.Config.GetString("jwt.secret")), nil
+	})
+	if err != nil {
+		return TokenData{}, err
+	}
+	if !token.Valid {
+		// jwt.Parse 通过而 Valid 为 false 不是一个预期行为
+		// 这里做一个特殊的错误返回
+		return TokenData{}, jwt.ErrTokenNotValidYet
+	}
+	return claims.TokenData, nil
 }
