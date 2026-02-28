2176a37b225ae8604403f2af9ce74823b899879e
MucheXD <68536431+MucheXD@users.noreply.github.com>
2026-02-28 15:57:59 +0800

    build: 前端构建 (交由后端管理)

--- cmd/deploy/redeploy.sh
+++ cmd/deploy/redeploy.sh
@@ -12,10 +12,20 @@ echo "当前工作目录: $(pwd)"
 echo "$(date) 终止现有容器..."
 docker compose down --remove-orphans
 
-echo "$(date) 拉取最新代码"
+# 拉取后端代码
+echo "$(date) 拉取后端最新代码"
 git pull
+git checkout "main"
 git reset --hard origin/main
 
+#拉取前端代码 (构建交由后端管理)
+echo "$(date) 拉取前端最新代码"
+cd ../../frontend/WSCVenueBookingFrontend
+git pull
+git checkout "deploy"
+git reset --hard origin/deploy
+
+cd ../../backend/WSCVenueBookingBackend
 echo "$(date) 正在执行后台构建与运行..."
 docker compose up --build -d
 
