ee264b459fedc118a90c1623f2ca5653276003f0
sqb550 <3180607552@qq.com>
2026-03-07 20:57:09 +0800

    fix: 定时任务bug修复

--- internal/utils/cron.go
+++ internal/utils/cron.go
@@ -10,7 +10,9 @@ import (
 
 func ScheduleRelease() {
 	now := time.Now()
-	result := database.DB.Model(&repository.NotificationContentEntity{}).Where("status=? AND release_time<=?", 2, now).Update("status", 1)
+	utcNow:=now.UTC()
+	utcNowStr:=utcNow.Format("2006-01-02 15:04:05")
+	result := database.DB.Model(&repository.NotificationContentEntity{}).Where("status=? AND release_time<=?", 2, utcNowStr).Update("status", 1)
 	err := result.Error
 	if err != nil {
 		log.Printf("Error upload notifications: %v\n", err)
